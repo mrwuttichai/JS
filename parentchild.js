5d5f7de7ef34398ab0388345b11dbe8b31422ac7
WUTTICHAI <mr.wuttichai.ktsm@gmail.com>
2023-03-21 17:43:22 +0700

    DOM

--- parentchild.js
+++ parentchild.js
@@ -34,3 +34,41 @@ addForm.addEventListener("submit", function (e) {
   li.appendChild(span2);
   value.value = "";
 });
+
+
+//delete
+const list = document.querySelector('#reading-list ul');
+list.addEventListener('click',function(e){
+    if (e.target.className == 'delete'){
+        const li =e.target.parentElement;
+        list.removeChild(li);
+    }
+})   
+
+// //Interacting with Form -- add readinglist
+const addForm = document.forms['add-list'];
+addForm.addEventListener('submit',function(e){
+    e.preventDefault(); // prevent from refresh
+    const value = addForm.querySelector('input[type="text"]').value; //add new reading list in the text box
+   // console.log(value);
+
+// //create element
+const li = document.createElement('li');
+const ReadingListName = document.createElement('span');
+const deleteBtn = document.createElement('span');
+
+// //add content
+deleteBtn.textContent = 'delete';
+ReadingListName.textContent = value; 
+
+// //add classes
+ReadingListName.classList.add('name');
+deleteBtn.classList.add('delete');
+
+
+// //append to DOM
+li.appendChild(ReadingListName);
+li.appendChild(deleteBtn);
+list.appendChild(li); //create but not context 
+
+});
